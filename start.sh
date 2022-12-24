932f1fca4fade3fdb48f721eb74fc60f994eb1b6
sigmamale007 <120267835+sigmamale007@users.noreply.github.com>
2022-12-24 11:43:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/sigmamale007/AyushMdisk.git /AyushMdisk
-cd /LazyUrlHunterrBOT
+cd /AyushMdisk
 pip3 install -U -r requirements.txt
 echo "⚡️⚡️ Starting AyushMdisk..."
 python3 main.py
